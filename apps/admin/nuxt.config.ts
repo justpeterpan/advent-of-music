06ce35d64996dea3da5e57ca62ca9e6e862c2de2
justpeterpan <peter.busch@posteo.net>
2023-10-17 01:12:09 +0200

    chore(deps): run pnpm audit fix for undici and babel/traverse

--- apps/admin/nuxt.config.ts
+++ apps/admin/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: '../../packages/base',
-  devtools: { enabled: true },
   runtimeConfig: {
     spotify: {
       clientId: process.env.SPOTIFY_CLIENT_ID,
