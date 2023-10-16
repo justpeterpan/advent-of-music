33744a37cc3bf07e73b2c66321d46a95a0cc47d2
justpeterpan <peter.busch@posteo.net>
2023-10-17 00:54:14 +0200

    feat(admin): add theme picker

--- apps/admin/app.vue
+++ apps/admin/app.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { useClipboard } from '@vueuse/core'
+
 const { copy } = useClipboard()
 const playlistId = ref('')
 const error = ref(false)
 const loading = ref(false)
 const playlistItems = ref()
+const selectedTheme = ref('dark')
 
 function extractSpotifyPlaylistId(input: string): string | null {
   const idPattern = /^[0-9A-Za-z]{22}$/
@@ -69,6 +71,10 @@ watch(playlistId, (newValue) => {
           class="rounded-md p-2 my-2 border-2 border-black shadow-black ring-2 ring-white text-black"
           :class="{ 'ring-rose-500': error }"
         />
+        <section class="mb-4">
+          <h2 class="text-xs font-extralight my-2">Theme</h2>
+          <ThemePicker v-model="selectedTheme" />
+        </section>
         <button
           type="submit"
           :disabled="!playlistId"
@@ -92,19 +98,19 @@ watch(playlistId, (newValue) => {
         <div v-else-if="!loading && playlistItems?.length">
           <div class="w-80 p-2 mb-1 border border-neutral-900 truncate">
             <NuxtLink
-              :to="`https://advent-of-music-calendar.vercel.app/cals/${playlistId}`"
+              :to="`https://advent-of-music-calendar.vercel.app/cals/${playlistId}?t=${selectedTheme}`"
               >{{
-                `https://advent-of-music-calendar.vercel.app/cals/${playlistId}`
+                `https://advent-of-music-calendar.vercel.app/cals/${playlistId}?t=${selectedTheme}`
               }}</NuxtLink
             >
           </div>
           <div
             @click="
               copy(
-                `https://advent-of-music-calendar.vercel.app/cals/${playlistId}`
+                `https://advent-of-music-calendar.vercel.app/cals/${playlistId}?t=${selectedTheme}`
               )
             "
-            class="w-80 p-2 border border-neutral-900 truncate"
+            class="w-80 p-2 border border-neutral-900 truncate cursor-pointer"
           >
             Click to copy url
           </div>
---
+++ apps/admin/components/ThemePicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grid grid-cols-2 gap-2">
+    <label
+      for="dark"
+      class="border border-white rounded-lg overflow-hidden focus-within:ring-2 focus-within:ring-blue-600 focus-within:border-blue-600"
+      :class="{ 'ring-2 ring-blue-600 border-blue-600': selected === 'dark' }"
+    >
+      <img :src="`dark.jpg`" alt="" class="object-cover rounded-lg" />
+      <input
+        type="radio"
+        id="dark"
+        class="absolute w-0 h-0 p-0 overflow-hidden border-none whitespace-nowrap"
+        v-model="selected"
+        value="dark"
+      />
+    </label>
+    <label
+      for="light"
+      class="border border-white rounded-lg overflow-hidden focus-within:ring-2 focus-within:ring-blue-600 focus-within:border-blue-600"
+      :class="{ 'ring-2 ring-blue-600 border-blue-600': selected === 'light' }"
+    >
+      <img :src="`light.jpg`" alt="" class="object-cover rounded-lg" />
+      <input
+        type="radio"
+        id="light"
+        class="absolute w-0 h-0 p-0 overflow-hidden border-none whitespace-nowrap"
+        v-model="selected"
+        value="light"
+      />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits()
+const props = defineProps<{ modelValue: string }>()
+const selected = ref(props.modelValue)
+
+watch(selected, (newValue) => {
+  emit('update:modelValue', newValue)
+})
+</script>
